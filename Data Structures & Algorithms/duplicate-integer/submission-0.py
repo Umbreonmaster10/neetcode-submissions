1befc6cb61e201ec14e4a04259e5104d409b7273
thing <163460030+Umbreonmaster10@users.noreply.github.com>
2026-05-12 13:02:21 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        lookup = {}
+        for num in nums:
+
+            if num in lookup:
+                return True
+            else:
+                lookup[num] = 1
+
+        return False
+
